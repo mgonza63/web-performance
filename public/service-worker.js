4107c17f07ff0da3d548baa958e25721e025a8ae
Mauricio Gonzalez <mauglezg21@gmail.com>
2020-04-17 18:53:44 -0500

    added more files to cache and created webpack.config.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,12 @@
 const FILES_TO_CACHE = [
     '/',
     '/index.html',
-]
+    '/styles.css',
+    '/manifest.webmanifest',
+    '/index.js',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+  ]
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
